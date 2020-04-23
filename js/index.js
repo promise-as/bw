a0e8c0e71110fe4afb9f077bee9173a844c6b19d
Hong Baojin <13809735212.163.com>
2020-04-23 18:50:34 +0800

    bug fix 1

--- js/index.js
+++ js/index.js
@@ -8,35 +8,42 @@ $('.closed').click(function () {
   $('.mask').hide();
 });
 
+// 鼠标点击
+// contBox: 内容盒子,
+function mouseClick(headBox, contBox, className) {
+  $(headBox).each(function (i) {
+    var that = this;
+    $(that).click(function () {
+      $(that).addClass(className).siblings().removeClass(className);
+      $($(contBox)[i]).addClass(className).siblings().removeClass(className);
+    })
+  })
+}
+
 // DOM（不包含图片）加载完之后执行
 $(function () {
+  // 关闭区块
+  function closeHandle(closeBtn, closeBox) {
+    $(closeBtn).click(function () {
+      $(closeBox).hide();
+    })
+  }
+  // 首页 正在直播
+  closeHandle($('.bw-live .live-close'), $('.bw-live'));
+
   // 切换 类
   // aimEle: 目标元素，className: 类名
   function switchClass(aimEle, className) {
-
-    if ($(aimEle).hasClass(className)) {
-
-      $(aimEle).removeClass(className);
-
-    } else {
-
-      $(aimEle).addClass(className)
-
-    }
+    $(aimEle).toggleClass(className);
   }
 
   // 展开与收起
   // outerBox: 最外层盒子，headBox: 头部盒子
   function unfold(outerBox, headBox, className) {
-
     $(outerBox).each(function () {
-
       var that = this;
-
       $(that).find(headBox).click(function () {
-
         switchClass(that, className);
-
       })
     })
   }
@@ -46,6 +53,9 @@ $(function () {
   unfold('.r-price .p-ticket', '.h-btn', 'on');
   // 课程终端 选择课程
   unfold('.t-right .c-detail', '.d-btn', 'on');
+  // 课程终端 选择课程 具体课程按钮
+  // unfold('.c-detail .c-detail', 'li', 'on');
+
   // 课程终端 试听单元
   unfold('.d-listen li', '.l-head', 'on');
   // 每日一练终端 收藏i
@@ -54,17 +64,10 @@ $(function () {
   // 鼠标移入
   // contBox: 内容盒子,
   function mouseEnter(headBox, contBox, className) {
-
     $(headBox).each(function (i) {
-
       var that = this;
-
-      // console.log(that)
-
       $(that).mouseenter(function () {
-
         $(that).addClass(className).siblings().removeClass(className);
-
         $($(contBox)[i]).addClass(className).siblings().removeClass(className);
       })
     })
@@ -80,36 +83,8 @@ $(function () {
   // 每日一练 扫一扫
   mouseEnter('.sys .sys-item', '.sp .sp-item', 'on');
 
-  // 鼠标点击
-  // contBox: 内容盒子,
-  function mouseClick(headBox, contBox, className) {
-    
-    $(headBox).each(function (i) {
-
-      var that = this;
-
-      $(that).click(function () {
-
-        $(that).addClass(className).siblings().removeClass(className);
-
-        $($(contBox)[i]).addClass(className).siblings().removeClass(className);
-      })
-    })
-  }
   // 首页 类别
   mouseClick('.class-cont .c-item', null, 'on');
   // 订单中心 支付方式
   mouseClick('.p-detail li', null, 'on');
-  // 每日一练终端 做题
-  mouseClick('.zt-item li', null, 'on');
-  
-
-  // 关闭区块
-  function closeHandle(closeBtn, closeBox) {
-    $(closeBtn).click(function () {
-      $(closeBox).hide();
-    })
-  }
-  // 首页 正在直播
-  closeHandle($('.bw-live .live-close'), $('.bw-live'));
 });
